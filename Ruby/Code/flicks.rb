4f6eb8dd5948377fda15e88b32537a430b8fdd60
g16bogner <g16bogner@gmail.com>
2015-09-17 19:47:35 +0200

    aufgabe3

--- Ruby/Code/flicks.rb
+++ Ruby/Code/flicks.rb
@@ -1,12 +1,21 @@
-def movie_listing(title, rank=20)
-	"Movie: #{title.capitalize} hat eine Bewertung von #{rank} am #{weekday}"
+class Movie
+	def initialize(ptitle, prank=50)
+		@title = ptitle.capitalize
+		@rank = prank
+	end
+	def thumbs_up
+		@rank = @rank + 10
+	end
+	def thumbs_down
+		@rank = @rank -10
+	end
+	def to_s
+		"Der Film #{@title} hat den Rank: #{@rank}"
+	end
 end
 
-def weekday
-	current_time = Time.new
-	current_weekday = current_time.strftime("%A") 
-end
-
-puts movie_listing("batman",50)
-puts movie_listing("superman")
+movie1 = Movie.new("batman", 40)
+puts movie1.to_s
+movie1.thumbs_up
+puts "Neuer Wert: #{movie1}"
 
