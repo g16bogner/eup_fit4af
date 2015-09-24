af18430d8496dd0d0fd0bd6f9b4b40c0c48dc549
g16bogner <g16bogner@gmail.com>
2015-09-24 19:05:34 +0200

    Eigenschaften

--- Ruby/Code/flicks.rb
+++ Ruby/Code/flicks.rb
@@ -1,7 +1,15 @@
 class Movie
+	attr_reader :title, :rank
+
 	def initialize(ptitle, prank=50)
 		@title = ptitle.capitalize
 		@rank = prank
+	end
+	def title=(title)
+		@title = title.capitalize
+	end
+	def normalized_rank
+		@rank / 100
 	end
 	def thumbs_up
 		@rank = @rank + 10
@@ -10,12 +18,12 @@ class Movie
 		@rank = @rank -10
 	end
 	def to_s
-		"Der Film #{@title} hat den Rank: #{@rank}"
+		"Der Film #{@title} hat den Rank: #{normalized_rank}"
 	end
 end
 
 movie1 = Movie.new("batman", 40)
-puts movie1.to_s
-movie1.thumbs_up
-puts "Neuer Wert: #{movie1}"
+puts movie1.title
+movie1.title = "Superman"
+puts movie1
 
--- Ruby/Code/studio_game.rb
+++ Ruby/Code/studio_game.rb
@@ -1,21 +1,28 @@
-# Erzeugen Sie folgende Ausgabe:
-# Larry has a health of 60.
-# CURLY has a health of 125.
-# *************Moe has a health of 100.*************
-# *************Moe has a health of 100.*************
-# Shemp......................... 90 health
-# Shemp......................... 90 health
-# Players:
-#         Larry
-#         Curly
-#         Moe
-# The game started on Thursday 08/02/2012 at 11:42AM
+class Player
+	def initialize(pname, phealth=100)
+		@name = pname
+		@health = phealth
+	end
+	def to_s
+		"Der Spieler #{@name} hat #{@health} Punkte"
+	end
+	def blam
+		@health = @health + 10
+		puts "#{@name} got blamed"
+	end
+	def w00t
+		@health -= 10
+		puts "#{@name} got w00ted"
+	end
+end
 
-name1 = 'larry'
-health1 = 60
+player1 = Player.new('Theo', 99)
+player2 = Player.new('Peter')
 
-puts "#{name1.capitalize} has a health of #{health1}"
-
-puts "#{name1.capitalize} has a health of #{health1}".center(40, '*')
-
-puts "#{name1.capitalize.ljust(30,'.')} #{health1} health"
+puts player2
+puts player2.name
+player2.w00t
+player2.w00t
+puts player2
+player1.blam
+puts player1
