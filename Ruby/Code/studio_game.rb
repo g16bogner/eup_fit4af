5a1652f0fcf05e25da4dd3aa6ab37edd4db2871b
g16bogner <g16bogner@gmail.com>
2015-10-15 17:51:55 +0200

    hash

--- Ruby/Code/studio_game.rb
+++ Ruby/Code/studio_game.rb
@@ -1,3 +1,27 @@
+class Game
+
+	def initialize(title)
+		@title = title
+		@player = []
+	end
+	def add_player(player)
+		@player << player
+	end
+	def play
+		puts "There are #{@player.count} players in #{@title}"
+		@player.each do |p|
+			puts "iN game.play aufgerufene KlASSE: #{p.class} id PLAYER1: #{p.object_id}"
+			puts p
+		end
+		@player.each do |p|
+			p.blam
+			p.w00t
+			p.w00t
+			puts p
+		end
+	end
+end
+
 class Player
 	attr_accessor :name
 	attr_reader :health
@@ -21,13 +45,19 @@ class Player
 		puts "#{@name} got w00ted"
 	end
 end
+game = Game.new("Knuckleheads")
 
 player1 = Player.new('Theo', 99)
+puts "KlASSE: #{player1.class} id PLAYER1: #{player1.object_id}"
+game.add_player(player1)
+
 player2 = Player.new('Peter')
 player3 = Player.new('Ludmilla')
 players = [player1, player2, player3]
 
-puts "Anzahl Spieler: #{players.count}"
+#puts "Anzahl Spieler: #{players.count}"
 players.each do |player|
-	puts player
+	#puts player
+	game.add_player(player)
 end
+game.play
---
+++ Ruby/Code/test.rb
@@ -0,0 +1,4 @@
+	hash_var3 = { drittens: "Talisker", viertens: "Laphroig" }
+	hash_var3.each do |key,val|
+		puts "Schlüssel: #{key} und Wert: #{val}"
+	end
