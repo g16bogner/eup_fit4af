8c2991e8d8effd592ae7d066ae3ac27ce1db9474
g16bogner <g16bogner@gmail.com>
2016-04-14 17:40:49 +0200

    authorisation

--- Rails/Projekte/rails_app/app/controllers/application_controller.rb
+++ Rails/Projekte/rails_app/app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
 
   before_action :set_sidebar_events
 
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
   private
 	  def set_sidebar_events
 			@sidebar_events = Event.upcoming
--- Rails/Projekte/rails_app/config/routes.rb
+++ Rails/Projekte/rails_app/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  
-  resources :users
   root "events#index"
+  get 'signup' => 'users#new'
+  get 'signin' => "sessions#new"
+
+  resource :session
+  resources :users
+
   resources :events do
     resources :registrations
   end
-    
-
-  
 end
